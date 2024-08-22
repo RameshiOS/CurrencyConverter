3c84c2aa0a7b0fa8ed1a66d83025931f8a29b247
RameshIOS <ramesh.guddala@gmail.com>
2024-08-22 20:29:28 +0530

    Initial Commit

---
+++ ExchangeCurrency/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExchangeCurrency
+//
+//  Created by Ramesh Guddala on 22/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExchangeCurrency/ExchangeCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeCurrencyApp.swift
+//  ExchangeCurrency
+//
+//  Created by Ramesh Guddala on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
